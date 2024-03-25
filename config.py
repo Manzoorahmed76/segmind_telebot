9d4f93d085a5d6d15a26d827cf723a56f9259ad5
Bablu jani <160379580+Manzoorahmed76@users.noreply.github.com>
2024-03-26 02:17:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-main_token = 'main_bot_token'
+main_token = '6934446943:AAHKV_pZ8PwmqJGIoTIMqUs4ItehZ5J6pW0'
 
-monitor_token = 'monitor_bot_token'
+monitor_token = '6934446943:AAHKV_pZ8PwmqJGIoTIMqUs4ItehZ5J6pW0'
 
-segmind_api_key = "Your_api_key"
+segmind_api_key = "SG_d29a16ed41d3bbcd"
 
-chat_id = "your_chat_id" 
+chat_id = "6644064144" 
